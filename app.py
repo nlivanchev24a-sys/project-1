2c7fb2987305b4c52e4662a1d627ee5045baa8d5
nlivanchev24a-sys <nlivanchev24a@gmail.com>
2026-03-09 12:15:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,55 @@
-st.title(" Оценки на ученици ")
+import streamlit as st
+st.title("My mini library app")
+if books not in st.session_state:
+  st.session_state.books = []
+  st.header("Add book")
+  title = st.text_input("title")
+  author = st.text_imput("Author")
+  price = st.text_imput("Price", min_value=0.0)
+  if st.button("Add book"):
+    book = { "title" : title,
+            "author" : author,
+            "price" : price}
+    st.sessipn_state.books.appened(book)
+    st.success("The book is added")
 
-if "grades" not in st.session_state:
-    st.session_state.grades =
- {
-        "Иван": [],
-        "Мария": [],
-        "Георги": []
-    }
+if st.button("Show all books"):
+  if len(st.session_state.books) == 0:
+    st.write("No added books")
+  else:
+    for book in st.session_state.books:
+      st.write("Tite", book["title"])
+      st.write("Author", book["author"])
+      st.write("Price", book["price"])
+      st.write("--------------------")
 
-student = st.selectbox("Избери ученик", st.session_state.grades.keys())
-grade = st.selectbox("Избери оценка", [2, 3, 4, 5, 6])
-
-if st.button(" Запиши оценка "):
-    st.session_state.grades[student].append(grade)
-    st.success("Оценката е записана")
-
-st.divider( )
-st.subheader(" Статистика ")
-
-if st.session_state.grades[student]:
-    df = pd.DataFrame( st.session_state.grades[student], columns=["Оценка"] )
-
-    st.write("Оценки на ученика")
-    st.bar_chart(df["Оценка"].value_counts().sort_index())
-
-    avg = sum(st.session_state.grades[student]) / len(st.session_state.grades[student])
-    st.write(f"Среден успех: **{avg:.2f}**")
-else:
-    st.write("Няма въведени оценки")
-
+st.header("Serch by author")
+search_author = st.text_input("Enter Author name:")
+if st.button("Serch by author"):
+  found = False
+for book in st.session_state.books:
+  if book["author"] == search_author:
+    st.write(book)
+    found = true
+  in found = False:
+  st.write("There are no books from this author")
+st.header("Search by Title")
+search_title = st.text_input("Enter title")
+if st.button("Serch by Title):
+             found = False
+for book in st.session_state.books:
+  if book["title"] == search_title:
+    st.write(book)
+    found = true
+  in found = False:
+  st.write("There are no books from this title")
+if st.button ("Show the cheapest book"):
+  if len (st.session_state.books) == 0:
+    st.write("There's no book")
+  else:
+    cheapest = st.session-state.books[0]
+for book in st.session_state.books:
+  if book["price"] < cheapest["price"]:
+    cheapest = book
+st.write("The cheapest book")
+st.write(cheapest)
